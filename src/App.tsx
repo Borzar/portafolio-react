9ccce57ea22951d4403c4a20237614a153b8313a
Boris <boris.zlobos@gmail.com>
2023-03-07 11:21:55 -0300

    fix navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,42 +5,9 @@ import { Portfolio } from './pages/Portfolio'
 import { Technologies } from './pages/Technologies'
 
 function App() {
-  const [aboutLink, setAboutLink] = useState('no-active')
-  const [experienceLink, setExperiencieLink] = useState('no-active')
-  const [technologiesLink, setTechnologiesLink] = useState('no-active')
-  const [portfolioLink, setPortfolioLink] = useState('no-active')
-
-  const activePortfolioLink = () => {
-    setAboutLink('no-active')
-    setExperiencieLink('no-active')
-    setTechnologiesLink('no-active')
-    setPortfolioLink('active')
-  }
-
-  const activeTechnologiesLink = () => {
-    setAboutLink('no-active')
-    setExperiencieLink('no-active')
-    setPortfolioLink('no-active')
-    setTechnologiesLink('active')
-  }
-
-  const activeExperiencieLink = () => {
-    setTechnologiesLink('no-active')
-    setPortfolioLink('no-active')
-    setAboutLink('no-active')
-    setExperiencieLink('active')
-  }
-
-  const activeAboutLink = () => {
-    setExperiencieLink('no-active')
-    setTechnologiesLink('no-active')
-    setPortfolioLink('no-active')
-    setAboutLink('active')
-  }
-
   return (
     <>
-      <nav className="navbar-vertical">
+      <nav className="navbar-vertical" id="main-navbar">
         <ul className="navbar-vertical-container">
           <li className="navbar-vertical-item">
             <img
@@ -50,41 +17,25 @@ function App() {
             ></img>
           </li>
           <li className="navbar-vertical-item">
-            <a className={aboutLink} onClick={activeAboutLink} href="#about">
-              Sobre mi
-            </a>
+            <a href="#about">Sobre mi</a>
           </li>
           <li className="navbar-vertical-item">
-            <a
-              className={experienceLink}
-              onClick={activeExperiencieLink}
-              href="#experience"
-            >
-              Experiencia
-            </a>
+            <a href="#experience">Experiencia</a>
           </li>
           <li className="navbar-vertical-item">
-            <a
-              className={technologiesLink}
-              onClick={activeTechnologiesLink}
-              href="#technologies"
-            >
-              Tecnologías
-            </a>
+            <a href="#technologies">Tecnologías</a>
           </li>
           <li className="navbar-vertical-item">
-            <a
-              className={portfolioLink}
-              onClick={activePortfolioLink}
-              href="#porfolio"
-            >
-              Portafolio
-            </a>
+            <a href="#porfolio">Portafolio</a>
           </li>
         </ul>
       </nav>
 
-      <section className="main-section">
+      <section
+        className="main-section"
+        data-bs-spy="scroll"
+        data-bs-target="#main-navbar"
+      >
         <section className="resume-section" id="about">
           <div className="resume-section-content">
             <About />
